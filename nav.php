82c9cfcfaaa2df059ec4f15c319bb5b8cd030be0
ella12-sudo <e.gonzales.562214@umindanao.edu.ph>
2026-03-03 10:30:07 +0800

    update changes

--- nav.php
+++
@@ -1,8 +0,0 @@
-<div class="navbar">
-    <h2>Student System</h2>
-    <div>
-        <a href="home.php">Home</a>
-        <a href="create_student.php">Add Student</a>
-        <a href="../logout.php">Logout</a>
-    </div>
-</div>
